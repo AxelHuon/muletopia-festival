cd7c7452ed93dc778148c8c244bf5c0760a3c8b1
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-01-30 10:40:14 +0100

    [feat/loader-animation][feat]:add styled components

---
+++ layout/LoaderLayout/LoaderLayout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.section`
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const LoaderLayout = () => {
+  return <div></div>;
+};
+
+export default LoaderLayout;
