6ee999fe68be830d3ff4b58597a304b0a59f727f
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-02-01 11:50:04 +0100

    fix conflicts

--- src/app/get-your-mullet/page.js
+++ src/app/get-your-mullet/page.js
@@ -1,12 +1,12 @@
 'use client';
-import Link from "next/link";
+import Link from 'next/link';
 import styled from 'styled-components';
 import { Colors } from '@/theme/Colors';
-import Image from "next/image";
-import {useEffect, useRef} from "react";
+import Image from 'next/image';
+import { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import { Draggable } from 'gsap/Draggable';
-import React from "react";
+import React from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
 import html2canvas from 'html2canvas';
 
@@ -34,7 +34,7 @@ const PageContent = styled.div`
     z-index: 10;
     pointer-events: none;
   }
-  
+
   button {
     background-color: ${Colors.white};
     color: ${Colors.primaryColor};
@@ -46,9 +46,7 @@ const PageContent = styled.div`
     cursor: pointer;
     z-index: 10;
     pointer-events: all;
-  
   }
-
 `;
 
 const Video = styled.video`
@@ -68,120 +66,140 @@ const HaircutImage = styled.img`
   position: absolute;
   width: 200px; // Vous pouvez ajuster la taille selon vos besoins
   height: auto;
-    top: -50vh;
+  top: -50vh;
   cursor: grab;
   transition: width 0.5s ease-in-out;
   z-index: 10;
 `;
 
 const ReturnLink = styled.a`
-    position: absolute;
-    top: 20px;
-    left: 40px;
-    font-family: 'Tanker';
-    font-size: 50px;
-    color: ${Colors.white};
-    text-align: center;
-    z-index: 3;
-    cursor: pointer;
+  position: absolute;
+  top: 20px;
+  left: 40px;
+  font-family: 'Tanker';
+  font-size: 50px;
+  color: ${Colors.white};
+  text-align: center;
+  z-index: 3;
+  cursor: pointer;
 `;
-
 
 gsap.registerPlugin(Draggable);
 export default function GetYourMullet() {
-    const videoRef = useRef(null);
-    const hairCutRef = useRef(null);
-    const animationRef = useRef(null);
-    const canvasRef = useRef(null);
-    const haircuts = ['/images/mullet-2.png', "/images/mullet-3.png"];
+  const videoRef = useRef(null);
+  const hairCutRef = useRef(null);
+  const animationRef = useRef(null);
+  const canvasRef = useRef(null);
+  const haircuts = ['/images/mullet-2.png', '/images/mullet-3.png'];
 
-    const takePhoto = () => {
-        html2canvas(document.body).then(canvas => {
-            const dataUrl = canvas.toDataURL('image/png');
-            const link = document.createElement('a');
-            link.href = dataUrl;
-            link.download = 'screenshot.png';
-            link.click();
-        });
-    };
+  const takePhoto = () => {
+    if (document) {
+      html2canvas(document.body).then((canvas) => {
+        const dataUrl = canvas.toDataURL('image/png');
+        const link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = 'screenshot.png';
+        link.click();
+      });
+    }
+  };
 
-    useEffect(() => {
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({ video: true })
-                .then(stream => {
-                    if (videoRef.current) {
-                        videoRef.current.srcObject = stream;
-                    }
-                })
-                .catch(err => console.error(err));
-        }
+  useEffect(() => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+          }
+        })
+        .catch((err) => console.error(err));
+    }
 
-        haircuts.forEach((haircut, index) => {
-            const randomX = Math.random() * 70 - 35;
-            gsap.fromTo(`.haircut-${index}`, {
-                x: `${randomX}vw`,
-            }, { y: '125vh', ease: 'power1.inOut', duration:2});
-        });
+    haircuts.forEach((haircut, index) => {
+      const randomX = Math.random() * 70 - 35;
+      gsap.fromTo(
+        `.haircut-${index}`,
+        {
+          x: `${randomX}vw`,
+        },
+        { y: '125vh', ease: 'power1.inOut', duration: 2 }
+      );
+    });
 
-        // Add drag and drop functionality
-        const haircutsElements = document.querySelectorAll('.haircut');
-        haircutsElements.forEach(haircut => {
-            haircut.draggable = true;
-            gsap.registerPlugin(Draggable);
-            Draggable.create(haircut, {
-                type: 'x,y',
-                bounds: window, // Change bounds to window
-                onDrag: function () {
-                    gsap.to(this.target, { rotation: this.x / 10, duration: 0.5 });
-                    gsap.to(animationRef.current, { x: this.x, y: this.y, duration: 0.5 })
-                },
-                onDragEnd: function () {
-                    gsap.to(this.target, { rotation: 0, duration: 0.5 });
-                    const haircutRect = haircut.getBoundingClientRect();
-                    const lottieAnim = document.querySelector('.lottie-player');
-                    lottieAnim.style.left = `${haircutRect.left - haircutRect.width / 2}px`;
-                    lottieAnim.style.top = `${haircutRect.top - haircutRect.height / 2}px`;
+    // Add drag and drop functionality
+    const haircutsElements = document.querySelectorAll('.haircut');
+    haircutsElements.forEach((haircut) => {
+      haircut.draggable = true;
+      gsap.registerPlugin(Draggable);
+      Draggable.create(haircut, {
+        type: 'x,y',
+        bounds: window, // Change bounds to window
+        onDrag: function () {
+          gsap.to(this.target, { rotation: this.x / 10, duration: 0.5 });
+          gsap.to(animationRef.current, {
+            x: this.x,
+            y: this.y,
+            duration: 0.5,
+          });
+        },
+        onDragEnd: function () {
+          gsap.to(this.target, { rotation: 0, duration: 0.5 });
+          const haircutRect = haircut.getBoundingClientRect();
+          const lottieAnim = document.querySelector('.lottie-player');
+          lottieAnim.style.left = `${haircutRect.left - haircutRect.width / 2}px`;
+          lottieAnim.style.top = `${haircutRect.top - haircutRect.height / 2}px`;
 
-                    animationRef.current.play(); // Play the Lottie animation
-                },
-            });
+          animationRef.current.play(); // Play the Lottie animation
+        },
+      });
 
-            // Add right click event listener to increase width
-            haircut.addEventListener('contextmenu', (e) => {
-                e.preventDefault();
-                const currentWidth = parseFloat(getComputedStyle(haircut).width);
-                haircut.style.width = `${currentWidth * 1.1}px`; // Increase width by 10%
-            });
+      // Add right click event listener to increase width
+      haircut.addEventListener('contextmenu', (e) => {
+        e.preventDefault();
+        const currentWidth = parseFloat(getComputedStyle(haircut).width);
+        haircut.style.width = `${currentWidth * 1.1}px`; // Increase width by 10%
+      });
 
-            haircut.addEventListener('wheel', (e) => {
-                e.preventDefault();
-                const rotation = gsap.getProperty(haircut, 'rotation');
-                gsap.to(haircut, { rotation: rotation + e.deltaY / 10, duration: 0.1 });
-            });
+      haircut.addEventListener('wheel', (e) => {
+        e.preventDefault();
+        const rotation = gsap.getProperty(haircut, 'rotation');
+        gsap.to(haircut, { rotation: rotation + e.deltaY / 10, duration: 0.1 });
+      });
+    });
+  }, []);
 
-
-
-        });
-
-    }, []);
-
-    return <main>
-        <PageContent>
-            <ReturnLink href="/">Return</ReturnLink>
-            <Video ref={videoRef} autoPlay />
-            {haircuts.map((haircut, index) => (
-                <HaircutImage key={index} ref={hairCutRef} className={`haircut haircut-${index}`} src={haircut} alt={`Haircut ${index + 1}`} />
-            ))}
-            <Player
-                ref={animationRef}
-                autoplay={false}
-                loop={false}
-                className={'lottie-player'}
-                src="/animations/stick-animation.json"
-                style={{ height: '350px', width: '350px', position: 'absolute', zIndex: 0 }}/>
-            <button onClick={takePhoto}>Take Photo</button>
-            <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
-            <p>Get your own mullet !</p>
-        </PageContent>
-    </main>;
+  return (
+    <main>
+      <PageContent>
+        <ReturnLink href="/">Return</ReturnLink>
+        <Video ref={videoRef} autoPlay />
+        {haircuts.map((haircut, index) => (
+          <HaircutImage
+            key={index}
+            ref={hairCutRef}
+            className={`haircut haircut-${index}`}
+            src={haircut}
+            alt={`Haircut ${index + 1}`}
+          />
+        ))}
+        <Player
+          ref={animationRef}
+          autoplay={false}
+          loop={false}
+          className={'lottie-player'}
+          src="/animations/stick-animation.json"
+          style={{
+            height: '350px',
+            width: '350px',
+            position: 'absolute',
+            zIndex: 0,
+          }}
+        />
+        <button onClick={takePhoto}>Take Photo</button>
+        <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
+        <p>Get your own mullet !</p>
+      </PageContent>
+    </main>
+  );
 }
