2e34a13d730410bbccdd34a991803071782794bd
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-02-01 13:25:30 +0100

    [feat/add-scroll-smoth][feat]:add scroll smoth

--- src/app/template.js
+++ src/app/template.js
@@ -1,3 +1,21 @@
+'use client';
+import gsap from 'gsap';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { useGSAP } from '@gsap/react';
+gsap.registerPlugin(ScrollSmoother);
+
 export default function Template({ children }) {
-  return <div>{children}</div>;
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+      effects: true, // looks for data-speed and data-lag attributes on elements
+      smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+    });
+  });
+
+  return (
+    <div id="smooth-wrapper">
+      <div id="smooth-content">{children}</div>
+    </div>
+  );
 }
